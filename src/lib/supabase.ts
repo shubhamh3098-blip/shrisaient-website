496bdd84454502611b43054a6d791978eae61056
shubhamh3098-blip <shubhamh3098@gmail.com>
2026-09-13 02:44:48 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://giwelvnyuqlfymoqppzn.supabase.co';
+const SUPABASE_URL = 'https://giwelvnyuqlfymoqppzn.supabase.co/rest/v1/';
 // Supabase Dashboard se copy ki gayi Publishable key yahan dalein
-const SUPABASE_ANON_KEY = 'sb_publishable_YOUR_KEY_HERE';
+const SUPABASE_ANON_KEY = 'sb_publishable_p5ZXWfakQwN-iQRG6uKHSQ_nq-LcpgE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
